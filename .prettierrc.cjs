469c88442760e3cea34f06abdd88818aff01ea3a
hiro1989 <hiro1989@example.com>
2024-03-06 20:08:50 +0900

    settings

---
+++ .prettierrc.cjs
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import("prettier").Config}
+ */
+const config = {
+  // Prettier is opinionated code formatter. Only add settings if you think you are smarter than the Prettier team.
+  semi: false,
+  plugins: ["prettier-plugin-organize-imports"],
+}
+
+module.exports = config
